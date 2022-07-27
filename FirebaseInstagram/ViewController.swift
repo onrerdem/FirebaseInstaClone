833ea07df6233aecd7cdc7bb08b6d0cd0ac5f706
onur erdem <onrerdem@icloud.com>
2022-07-27 15:23:01 +0300

    import firestore

--- FirebaseInstagram/ViewController.swift
+++ FirebaseInstagram/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var passwordTxt: UITextField!
+    
+    @IBOutlet weak var emailTxt: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signinBtn(_ sender: Any) {
+        performSegue(withIdentifier: "toTBC", sender: nil)
+    }
+    
+    @IBAction func signupBtn(_ sender: Any) {
+    }
 }
 
