95dcb2e16e34c9c8bcba498f47cbdc49b2dbf293
onur erdem <onrerdem@icloud.com>
2022-07-28 19:27:21 +0300

    firestorage

--- FirebaseInstagram/UploadViewController.swift
+++ FirebaseInstagram/UploadViewController.swift
@@ -6,24 +6,76 @@
 //
 
 import UIKit
+import FirebaseStorage
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
+    @IBOutlet weak var uploadBtn: UIButton!
+    @IBOutlet weak var commentLbl: UITextField!
+    @IBOutlet weak var ımageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        ımageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageSelected))
+        ımageView.addGestureRecognizer(gestureRecognizer)
+        
+        
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func imageSelected(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true)
+ 
     }
-    */
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        ımageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+        
+    }
+    
+    @IBAction func uploadBtn(_ sender: Any) {
+        let storage = Storage.storage()
+        let storageRef = storage.reference()
+        
+        let mediaFolder = storageRef.child("media")
+        
+        if let data = ımageView.image?.jpegData(compressionQuality: 0.5){
+            
+            let uuid = UUID().uuidString
+            let imageRef = mediaFolder.child("\(uuid).png")
+            
+            imageRef.putData(data) { metaData, error in
+                if error != nil {
+                    self.getAlert(errorInput: error!.localizedDescription)
+                }else{
+                    imageRef.downloadURL { url, error in
+                        if error != nil{
+                            self.getAlert(errorInput: error?.localizedDescription ?? "Error")
+                        }else{
+                            let imageUrl = url?.absoluteString
+                            print(imageUrl)
+                        }
+                        
+                    }
+                }
+            }
+        }
+            
+        
+    }
+    
+    func getAlert(errorInput : String) -> Void {
+        
+        let alert = UIAlertController(title: "Error", message: errorInput, preferredStyle: UIAlertController.Style.alert)
+        let alertBtn = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(alertBtn)
+        self.dismiss(animated: true)
+        
+    }
+    
 }
